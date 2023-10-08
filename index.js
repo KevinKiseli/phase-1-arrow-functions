66e940f6ebec05e66009c334206ff4924a00fb58
Kevin <kevin.kiseli@student.moringaschool.com>
2023-10-08 11:41:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const num1 = 2000
+const num2 = 100
+const num3 = 2
+const num4 = 5
+
+let divide = function(){
+    return(num1/num2);
+    
+}
+
+let square = function(){
+    return(num3*num3);
+}
+
+let add = function(){
+    return(num3+num4);
+}
